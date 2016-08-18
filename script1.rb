7ba49f01e7f0c29e6ed9af76080faa1f04d54f55
oleksandrbyk <obyk320@gmail.com>
2016-08-18 10:58:40 -0500

    Add files via upload

---
+++ script1.rb
@@ -0,0 +1,33 @@
+# Doing some simple Ruby here
+my_var = "Hello World"
+
+# Arithmetic functions
+my_answer = 3 * 2
+new_answer = 3 + my_answer
+newer_answer = new_answer % 2
+
+print "Hi everybody"
+put "Hi folks"
+print "Do you like bacon"
+
+# Doing a method to count string length
+"Brad Beiermann".length
+
+# Doing a reverse method
+"Brad Beiermann".reverse
+
+puts "Brad Beiermann".upcase  #converts to uppercase
+puts "HOWDY Brad".downcase  #converts to lower case
+
+=begin
+A multi-line comment.
+It can be many lines
+as long as begin and end are in place
+no space should be used in between begin and end
+=end
+
+# Chaining methods together
+name = "Brad Beiermann"
+name.downcase.reverse.upcase
+
+
