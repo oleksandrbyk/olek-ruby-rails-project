367796236744b3d1bd2249592a3916dafefe8cb9
oleksandrbyk <obyk320@gmail.com>
2016-09-18 11:24:41 -0500

    Add files via upload

--- script1.rb
+++ script1.rb
@@ -2,19 +2,20 @@
 my_var = "Hello World"
 
 # Arithmetic functions
-my_answer = 3 * 2
-new_answer = 3 + my_answer
-newer_answer = new_answer % 2
+my_answer = 3 * 2  # Output => 6
+new_answer = 3 + my_answer # Output => 9
+newer_answer = new_answer % 2  # Output => 1   Note: This 9 / 2 = 4 with remainder of 1
 
 print "Hi everybody"
 puts "Hi folks"
-print "Do you like bacon"
+print "Do you like bacon?"
 
 # Doing a method to count string length
 "Brad Beiermann".length
 
 # Doing a reverse method
 "Brad Beiermann".reverse
+# Output => nnamreieB darB
 
 puts "Brad Beiermann".upcase  #converts to uppercase
 puts "HOWDY Brad".downcase  #converts to lower case
@@ -28,6 +29,7 @@ no space should be used in between begin and end
 
 # Chaining methods together
 name = "Brad Beiermann"
-name.downcase.reverse.upcase
+puts name.downcase.reverse.upcase
+# Output => DARB
 
 
