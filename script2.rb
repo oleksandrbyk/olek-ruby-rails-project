578316af4f0bd49fb9691dcc58a211ab81f74911
oleksandrbyk <obyk320@gmail.com>
2016-09-18 12:06:43 -0500

    Add files via upload

--- script2.rb
+++ script2.rb
@@ -2,7 +2,7 @@
 
 print "What's your first name?"
 first_name = gets.chomp
-first_name.capitalize!
+first_name.capitalize!  # The exclamation means the original value of first_name can be changed. In this case, it is capatalized. 
 
 print "What's your last name?"
 last_name = gets.chomp
@@ -14,6 +14,6 @@ city.capitalize!
 
 print "What state or province are you from?"
 state = gets.chomp
-state.upcase!
+state.upcase!   # The exclamation means the original value of state can be changed. In this case, it is made uppercase. 
 
 puts "Your name is #{first_name} #{last_name} and you're from #{city}, #{state}!"
