77c8d3f0d7e3fe787fe75ec2257fa3cf2bd1ecf6
oleksandrbyk <obyk320@gmail.com>
2016-11-28 22:30:37 -0600

    Delete routes.rb

--- Rails-Project-Files/TravelApp/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-	get '/tags' => 'tags#index'
-	get '/tags/:id' => 'tags#show', as: :tag
-	get '/destinations/:id' => 'destinations#show', as: :destination
-	get '/destinations/:id/edit' => 'destinations#edit', as: :edit_destination 
-	patch '/destinations/:id' => 'destinations#update'
-end
